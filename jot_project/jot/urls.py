7bd4011cd91893c16faad30c0d978b6d3ac7826d
Connor <2470724c@student.gla.ac.uk>
2022-03-22 15:53:45 +0000

    refactored allowed_hosts

--- jot_project/jot/urls.py
+++ jot_project/jot/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('contactus/', views.contactus, name='contactus'),
     path('categories/', views.categories, name='categories'),
     path('surpriseme/', views.surpriseme, name='surpriseme'),
-    
+    path('book/', views.book, name='book'),
+
 ]
