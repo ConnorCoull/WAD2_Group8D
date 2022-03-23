119a9215797c6a0236f8e9bfbdbdc2b69d1c5229
Ythyang <2681610y@student.gla.ac.uk>
2022-03-23 11:15:32 +0800

    search modify

--- jot_project/jot/views.py
+++ jot_project/jot/views.py
@@ -3,12 +3,18 @@ from django.shortcuts import render
 #from .forms import UploadFileForm
 from django.db.models import Q
 from datetime import datetime
+from .models import Book
 
 # Imaginary function to handle an uploaded file.
 #from somewhere import handle_uploaded_file
 
 def index(request):
+    model = Book
+    keyword = request.GET.get('q')
     context_dict = {}
+    if keyword:
+       books_list = Book.objects.filter(book_title__icontains = keyword)
+       context_dict['books_list'] = books_list
     visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
     return render(request, 'jot/index.html', context=context_dict)
