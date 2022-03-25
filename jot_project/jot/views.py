d98026279d485ba9526cb31c1033a8fa08749acf
StudentChenyu <98562062+StudentChenyu@users.noreply.github.com>
2022-03-25 20:20:30 +0800

    Update views.py

--- jot_project/jot/views.py
+++ jot_project/jot/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from datetime import datetime
 from .GoogleBookSearchAPI import getRatings
+from django.contrib.auth.decorators import login_required
 
-#call the upload function in Forms
 from .forms import BookForm
 from django.conf import settings
 
@@ -136,8 +136,8 @@ def visitor_cookie_handler(request):
         request.session['last_visit'] = last_visit_cookie
 
     request.session['visits'] = visits
-#i
-#required login?
+    
+@login_required
 def upload_books(request):
     if request.method == 'POST' :
         form = BookForm(request.POST,request.FILES)
