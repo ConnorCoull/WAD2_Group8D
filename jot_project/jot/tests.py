784ac12023818eb6f884e7200e1574bfbc3deca7
Ythyang <99317484+Ythyang@users.noreply.github.com>
2022-03-28 02:15:02 +0800

    Update tests.py

--- jot_project/jot/tests.py
+++ jot_project/jot/tests.py
@@ -2,7 +2,8 @@
 #Otherwise use the unittest.TestCase class   
 from django.test import TestCase
 import unittest
-
+from .models import Book
+from .models import User
 from jot.GoogleBookSearchAPI import getRatings
 
 # Create your tests here.
@@ -17,3 +18,20 @@ class ApiSearchTestCase(unittest.TestCase):
         self.assertNotEqual(getRatings(1984), [0,0])
         #Normal search should renturn a non-deafult result 
         self.assertNotEqual(getRatings("Lord of the Rings"),[0,0])
+        
+    def test_models_books(self):
+        
+        keyword = "Inferno"
+        books_list = Book.objects.filter(book_title__icontains = keyword)
+        self.assertEqual(books_list[0].book_title,"Inferno")
+
+        keyword = "Inferno"
+        books_list = Book.objects.filter(book_title__icontains = keyword)
+        self.assertEqual(books_list[1].book_title,"Inferno2")
+
+
+    def test_models_users(self):
+
+        keyword = "OnionGuy34672"
+        users_list = User.objects.filter(username__icontains = keyword)
+        self.assertEqual(users_list[0].username,"OnionGuy34672")
